0b5889bf9a596ad19682f9d441b05493dd4819ea
Taint-Yan <yantiti1@163.com>
2024-09-11 23:24:43 +0800

    feat(svg图标显示 && 登录验证): svg 内部图标显示和外部图标显示 && 登录表单校验

--- src/main.js
+++ src/main.js
@@ -3,7 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import installElementPlus from './plugins/element'
+// 引入全局样式
+import './styles/index.scss'
+import installIcons from '@/icons' // 导入svgIcon
 
 const app = createApp(App)
 installElementPlus(app)
+installIcons(app)
 app.use(store).use(router).mount('#app')
---
+++ src/views/login/index.vue
@@ -0,0 +1,158 @@
+<template>
+  <div class="login-container">
+    <el-form class="login-form" :model="loginForm" :rules="loginRules">
+      <!-- 登录标题 -->
+      <div class="title-container">
+        <h3 class="title">用户登录</h3>
+      </div>
+      <!-- userName -->
+      <el-form-item prop="userName">
+        <span class="svg-container">
+          <!-- <el-icon>
+            <Avatar />
+          </el-icon> -->
+          <!-- <SvgIcon icon="https://res.lgdsunday.club/user.svg" /> -->
+          <svg-icon icon="user" />
+        </span>
+        <el-input
+          v-model="loginForm.userName"
+          name="userName"
+          placeholder="请输入用户名"
+          type="text"
+        ></el-input>
+      </el-form-item>
+      <!-- password -->
+      <el-form-item prop="password">
+        <span class="svg-container">
+          <!-- <el-icon> <Avatar /></el-icon> -->
+          <svg-icon icon="password" />
+        </span>
+        <el-input
+          v-model="loginForm.password"
+          name="password"
+          placeholder="请输入密码"
+        ></el-input>
+        <span class="show-pwd">
+          <el-icon>
+            <!-- <Avatar /> -->
+            <svg-icon icon="eye" />
+          </el-icon>
+        </span>
+      </el-form-item>
+      <!--登录按钮 -->
+      <el-button type="primary" style="width: 100%; margin-bottom: 30px"
+        >登录</el-button
+      >
+    </el-form>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { validatePassword } from './rules'
+// import SvgIcon from '@/components/SvgIcon/index.vue'
+// import { Avatar } from '@element-plus/icons'
+const loginForm = ref({
+  userName: 'super-admin',
+  password: '123456'
+})
+
+const loginRules = {
+  userName: [{ required: true, message: '请输入用户名', trigger: 'blur' }],
+  password: [
+    {
+      required: true,
+      trigger: 'blur',
+      validator: validatePassword()
+    }
+  ]
+}
+</script>
+
+<style lang="scss" scoped>
+$bg: #2d3a4b;
+$dark_gray: #889aa4;
+$light_gray: #eee;
+$cursor: #fff;
+.login-container {
+  width: 100%;
+  min-height: 100%;
+  background-image: url('../../../public/home.jpg');
+  background-size: cover;
+  background-repeat: no-repeat;
+  object-fit: fill;
+  overflow: hidden;
+  position: absolute;
+  // .login-container-one {
+  //   position: absolute;
+  //   inset: 0;
+  // background: linear-gradient(to right, rgb(25, 25, 27), rgb(56, 52, 53));
+  // }
+  .login-form {
+    position: relative;
+    width: 520px;
+    max-width: 100%;
+    padding: 0px 35px;
+    margin: 0 auto;
+    overflow: hidden;
+    background: $bg;
+    // background: transparent;
+    opacity: 0.9;
+    transform: translate(0, 50%); //垂直居中
+
+    ::v-deep .el-form-item {
+      border: 1px solid rgba(255, 255, 255, 0.1);
+      background: rgba(0, 0, 0, 0.1);
+      border-radius: 5px;
+      color: #454545;
+    }
+
+    ::v-deep .el-input {
+      display: inline-block;
+      height: 47px;
+      width: 85%;
+
+      input {
+        background: transparent;
+        border: 0px;
+        -webkit-appearance: none;
+        border-radius: 0px;
+        padding: 12px 5px 12px 15px;
+        color: $light_gray;
+        height: 47px;
+        caret-color: $cursor;
+      }
+    }
+  }
+  .svg-container {
+    padding: 6px 5px 6px 15px;
+    color: $dark_gray;
+    vertical-align: middle;
+    display: inline-block;
+  }
+
+  .title-container {
+    position: relative;
+
+    .title {
+      font-size: 26px;
+      color: $light_gray;
+      margin: 0px auto 40px auto;
+      text-align: center;
+      font-weight: bold;
+      padding-top: 30px;
+      box-sizing: border-box;
+    }
+  }
+
+  .show-pwd {
+    position: absolute;
+    right: 10px;
+    top: 7px;
+    font-size: 16px;
+    color: $dark_gray;
+    cursor: pointer;
+    user-select: none;
+  }
+}
+</style>
