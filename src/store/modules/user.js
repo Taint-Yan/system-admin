865e8f95df09a5063acb15acc13bed73feffed5f
Taint-Yan <yantiti1@163.com>
2024-09-12 22:03:49 +0800

    feat: 封装本地存储，和vuex结合使用

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,11 +1,20 @@
 import { login } from '@/api/sys'
+import { setItem, getItem } from '@/utils/storage'
 import md5 from 'md5'
+import TOKEN from '@/constant'
 
 // 登录操作
 export default {
   namespaced: true,
-  state: () => ({}),
-  mutations: {},
+  state: () => ({
+    token: getItem(TOKEN) || ''
+  }),
+  mutations: {
+    setToken(state, token) {
+      state.token = token
+      setItem(TOKEN, token)
+    }
+  },
   actions: {
     login(context, useInfo) {
       const { username, password } = useInfo
@@ -14,6 +23,7 @@ export default {
           username,
           password: md5(password)
         }).then(data => {
+          context.commit('user/setToken', data.data.data.token)
           resolve(data)
         }).catch(error => {
           reject(error)
---
+++ src/utils/storage.js
@@ -0,0 +1,46 @@
+/**
+ * @description: 本地存储
+ * 存储数据
+ */
+
+export const setItem = (key, value) => {
+  // 将数组、对象类型的数据转换为json字符串
+  if (typeof value === 'object') {
+    value = JSON.stringify(value)
+  }
+  window.localStorage.setItem(key, value)
+}
+
+/**
+ * @description: 存储数据
+ * 获取数据
+ */
+
+export const getItem = (key) => {
+  const value = window.localStorage.getItem(key)
+  try {
+    // 尝试将value转换为json对象
+    return JSON.parse(value)
+  } catch (error) {
+    // 如果转换失败则直接返回
+    return value
+  }
+}
+
+/**
+ * @description: 本地存储
+ * 删除数据
+ */
+
+export const removeItem = (key) => {
+  window.localStorage.removeItem(key)
+}
+
+/**
+ * @description: 本地存储
+ * 删除所有数据
+ */
+
+export const removeItemAll = () => {
+  window.localStorage.clear()
+}
