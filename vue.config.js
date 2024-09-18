0f76b676682a2b26823de513f9f933806068b155
Taint-Yan <yantiti1@163.com>
2024-09-18 15:19:52 +0800

    fix: 修复控制台警告

--- vue.config.js
+++ vue.config.js
@@ -35,5 +35,14 @@ module.exports = {
         symbolId: 'icon-[name]'
       })
       .end()
+    // 处理 __VUE_PROD_HYDRATION_MISMATCH_DETAILS__ 未定义警告
+    config.plugin('define').tap((definitions) => {
+      Object.assign(definitions[0], {
+        __VUE_OPTIONS_API__: 'true',
+        __VUE_PROD_DEVTOOLS__: 'false',
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false'
+      })
+      return definitions
+    })
   }
 }
